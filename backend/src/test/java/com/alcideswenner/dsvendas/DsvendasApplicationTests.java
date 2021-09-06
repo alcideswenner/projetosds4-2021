6863d68bbe315216a6178383b890a997c6777abf
Alcides Wenner <alcideswenner.jus@gmail.com>
2021-09-06 17:20:27 -0300

    projetosds3-2021

---
+++ backend/src/test/java/com/alcideswenner/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcideswenner.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
