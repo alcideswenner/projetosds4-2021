19e125077b3514f57063eca7be0eb5fd99b543be
Alcides Wenner <alcideswenner.jus@gmail.com>
2021-09-06 19:39:55 -0300

    Basic stati components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "componentes/DataTable";
+import Footer from "componentes/Footer";
+import NavBar from "componentes/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1>Ola mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá, mundo!</h1>
+      </div>
+      <DataTable />
+      <Footer />
+    </>
   );
 }
 
